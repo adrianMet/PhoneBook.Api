3bb07e03d317e2f86c645d3dbc0a8aeedf2af034
Adrian <adrianmetowski@sourceful.nl>
2023-05-22 19:27:26 +0200

    prepare for tests

---
+++ PhoneBook.Tests.Unit/Entities/ContactBookTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PhoneBook.Tests.Unit.Entities
+{
+    public class ContactBookTests
+    {
+        [Fact]
+        public void Test()
+        {
+            // ARRANGE
+
+            // ACT
+
+            // ASSERT
+        }
+
+        public ContactBookTests()
+        {
+            
+        }
+    }
+}
