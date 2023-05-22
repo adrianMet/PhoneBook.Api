16b9678cac9c29e6734f1650cdfdac46aab0d844
Adrian <adrianmetowski@sourceful.nl>
2023-05-22 23:00:36 +0200

    ready tests for ContactBook entity

--- PhoneBook.Tests.Unit/Entities/ContactBookTests.cs
+++ PhoneBook.Tests.Unit/Entities/ContactBookTests.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using PhoneBook.Api.Entities;
+using PhoneBook.Api.Exceptions;
+using Shouldly;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,18 +13,53 @@ namespace PhoneBook.Tests.Unit.Entities
     public class ContactBookTests
     {
         [Fact]
-        public void Test()
+        public void given_existing_contact_add_contact_should_fail()
         {
             // ARRANGE
+            _contactBook.AddContact(_contact);
 
             // ACT
+            var exception = Record.Exception(() => _contactBook.AddContact(_contact));
 
             // ASSERT
+
+            exception.ShouldNotBeNull();
+            exception.ShouldBeOfType<ContactAlreadyExists>();
         }
 
+        [Fact]
+        public void given_not_existing_contact_remove_contact_should_fail()
+        {
+            var exception = Record.Exception(() => _contactBook.RemoveContact(_contact));
+
+            exception.ShouldNotBeNull();
+            exception.ShouldBeOfType<ContactDoesntExists>();
+        }
+
+        [Fact]
+        public void given_not_existing_contact_add_contact_should_succeed()
+        {
+            _contactBook.AddContact(_contact);
+
+            _contactBook.Contacts.ShouldHaveSingleItem();
+        }
+
+        [Fact]
+        public void given_existing_contact_remove_contact_should_succeed()
+        {
+            _contactBook.AddContact(_contact);
+
+            _contactBook.RemoveContact(_contact);
+
+            _contactBook.Contacts.ShouldBeEmpty();
+        }
+
+        private readonly ContactBook _contactBook;
+        private readonly Contact _contact;
         public ContactBookTests()
         {
-            
+            _contactBook = new ContactBook("B1");
+            _contact = new Contact("John", "Lock", 123456789, "B1");
         }
     }
 }
